353644f1db2d94037638482a342a3185b78a3412
eugordinho <martinsanchezr@outlook.com>
2022-06-02 17:17:12 -0300

    agregando codigo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let total = 0;
+
+let nombrePagador;
+
+let monto = 0;
+
+let dividendo = 0;
+
+for(let i = 1; i < 10; ++i){
+    monto = prompt("tuvi");
+
+    total += monto;
+
+    dividendo = total / i;
+
+}
+
+console.log(dividendo);
